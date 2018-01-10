98577b9e5e9b98a5ba6b613a8f736ad2d3095c08
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-01-10 11:25:24 +0000

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	int number;
+ string s1="hello.123"
+ if(number!=null)
+ {
+ printf("count number %s",s1);
+ }
+ else
+ {
+ printf("print %s",s1);
+ }
+ }
+  
+  
+
+
