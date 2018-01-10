fd3e3f3f92f721c21c9d3b843488a8928e6b0097
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-01-10 10:54:04 +0000

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+	int count;
+  string s1;
+ s1="hello.world"
+
+{
+  if(s1!=null)
+  {
+  printf("count the number of line %s",s1);
+  }
+  else
+  {
+  printf("present the line %s",s1);
+  }
+  count++;
+  }
+  }
+  
+  
