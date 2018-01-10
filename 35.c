10c4e42b360929e362060412dc09b558e1ad0271
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-01-10 11:04:56 +0000

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+	int a[3]={1,2,3};
+  int i,count;
+  for(i=0;i<=5;i++)
+  {
+  if(a!=null)
+  {
+  printf("count the numeric %d",a);
+  }
+  else
+  {
+  printf("present the line %d",a);
+  }
+  count++;
+  }
+  
