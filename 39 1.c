d042b1f2694f79c8281fe69e7bd8b3e2d6406e2e
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-01-19 11:10:57 +0000

    Create 39 1.c

---
+++ 39 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,maximum;
+scanf("%d",a[i]);
+a[0]=maximum;
+for(i=0;i<a[i];i++)
+{
+if(a[i]>maximum)
+{
+maximum=a[i];
+printf("print the maximum value %d",maximum);
+}
+}
+}
