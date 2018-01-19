0362dc29877f0bca0aadbf175dc2c5742d4aa78d
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-01-19 11:27:49 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+char a[10];
+int i,n;
+printf("enter the string ");
+scanf("%d",&a);
+printf("how many time printed");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+	printf("%d",a);
+}
+}
