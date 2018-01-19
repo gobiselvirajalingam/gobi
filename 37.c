f7a0e8e0586ece3913cb36362b49eedcfed3a32e
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-01-19 14:21:30 +0530

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int x, y, t;
+ 
+   printf("Enter the value of x and y");
+   scanf("%d%d", &x, &y);
+ 
+   printf("Before Swapping x = %dy = %d",x,y);
+ 
+   t= x;
+   x    = y;
+   y    = t;
+ 
+   printf("After Swapping x = %dy = %d",x,y);
+ }
+
