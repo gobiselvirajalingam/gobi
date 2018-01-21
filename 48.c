0be581fbe1daa41e048cec72004559d7333cf2ba
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-01-21 15:13:59 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+int a,b,c, s;
+ float d;
+ printf( enter 3 numbers");
+scanf("%d%d%d",&a,&b,&c);
+ s=a+b+c;
+d=(a+b+c)/3;
+ printf("\nSum is %d", s);
+ printf("\nAverage is  %f",d);
+}
+
+
