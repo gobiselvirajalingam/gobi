25cb04a6aaa65e121b806b8ac4d9df381c896b71
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-01-31 16:14:18 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+int i = 65;
+int j = 120;
+printf(" value of i=%d j=%d before swapping", i, j);
+i = i ^ j;
+j = i ^ j;
+i = i ^ j;
+printf("value of i=%d j=%d after swapping", i, j);
+}
