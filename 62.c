8642657b46aa0dbb6a695f60ecafcefa0a6855f3
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-02-02 14:06:40 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a=01,b;
+printf("enter the number");
+scanf("%d",&b);
+if(a==b)
+{
+printf(" binary representation number");
+}
+else
+{
+printf("not binary number");
+}
+}
