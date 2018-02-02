2b885c6c3900bdc7ba9e1a73eb0d0a149b997e7d
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-02-02 15:04:48 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter number");
+scanf("%d",&a);
+if(a%6==0)
+{
+    printf("prime");
+    }
+    else
+    {
+        printf("not prime");
+    }
+}
