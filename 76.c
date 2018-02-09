b551f37dcf77f25d56e5915ca609813eff729fb0
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-02-09 14:39:44 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter number");
+scanf("%d",&n);
+if(n%6==0)
+{
+    printf("prime");
+    }
+    else
+    {
+        printf("composite number");
+    }
+}
