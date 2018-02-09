1a891be49b4213d43c415e1b717ae7a41e0ae922
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-02-09 15:05:50 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,n,c;
+    printf("enter two number");
+    scanf("%d%d",&a,&n);
+    c=a*n;
+    if(c%2==0)
+    {
+    printf("perfect squre");
+}
+else
+{
+    printf("not perfect squre");
+}
+}
