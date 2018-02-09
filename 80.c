29bd9ff8972d39e4a6578c659c9337c5de6371de
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-02-09 15:15:55 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two number");
+    scanf("%d%d",&a,&b);
+   
+    if((a%2!=0)&&(b%2!=0))
+    {
+    printf("odd number%d%d",a,b);
+}
+}
