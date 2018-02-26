e0660f97538fa8c30eba4139cf3b0920f88bb638
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-02-26 16:29:33 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int n,i;
+  char str[567];
+  printf("enter string");
+  scanf("%c",&str[i]);
+  for(i=0;str[i]!='\0';i++)
+  {
+  if(str[i]==(n>=0 && n<=9))
+  {
+      printf("%d",str[i]);
+  }
+}
+}
