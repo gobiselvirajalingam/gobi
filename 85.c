e0fa856bf966caa63fd194309b5f4bdf12bbcb38
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-02-26 16:05:20 +0530

    Create 85.c

---
+++ 85.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int i;
+  char a[5]={'g','f','i','y'};
+  
+  for(i=0;i<=a[i];i++)
+  {
+      if(a[i]%2==1)
+      {
+          printf("%c",a[i]);
+      }
+  }
+  for(i=0;i<=a[i];i++)
+{
+    if(a[i]%2==0)
+    {
+       printf("%c",a[i]); 
+    }
+}
+}
