89d3a8afd5b887d3f5a9bb62d660b9d6e66c894a
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-03-12 16:16:52 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c,fr;
+    printf("enter number");
+    scanf("%d%d%d",&a,&b,&c);
+    fr=a*b*c;
+    fr=fr/100;
+    printf("%d",fr);
+}
+
+    
