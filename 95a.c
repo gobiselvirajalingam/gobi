ee575e45657b064fc912fd4a139530a77b3aa0d3
gobiselvirajalingam <30829587+gobiselvirajalingam@users.noreply.github.com>
2018-03-12 16:26:57 +0530

    Rename 95.c to 95a.c

---
+++ 95a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    float a,b,c,fr;
+    printf("enter number");
+    scanf("%f%f%f",&a,&b,&c);
+    fr=(a*b*c)/100;
+    
+    printf("%f",fr);
+}
+
+    
